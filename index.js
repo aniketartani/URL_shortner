a730fc172f48c47146323b9232d289569be48d48
Raj <75741866+rajdama@users.noreply.github.com>
2021-06-05 19:14:34 +0530

    Made dynamic

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-var shortUrl = require('node-url-shortener');
+const express = require('express');
+const app = express();
+const path = require('path');
+const shortUrl = require('node-url-shortener');
+let port = 80;
 
-shortUrl.short('https://github.com/aniketartani', function(err, url){
-    console.log(url);
-});
+//EXPRESS RELATED STUFF
+app.use('/static', express.static('static'));//Serving the static files
+app.use(express.urlencoded());
+
+//PUG RELATED STUFF
+app.set('view engine', 'pug');// Set the template engine as pug
+app.set('views', path.join(__dirname, 'views'));//Set the view/template directory (we could also do this without path module)
+
+//ENDPOINTS
+
+app.post('/', (req, res) => {
+    myurl = req.body.myurl;
+    shortUrl.short(`${myurl}`, function (err, url) {
+        console.log(url);
+        let a = url;
+        const params = {'url':`${a}`};
+    
+    res.status(200).render('index',params)});
+})
+
+
+//START THE SERVER
+app.listen(port, () => {
+    console.log(`The application is started successfully on port ${port}`)
+})
